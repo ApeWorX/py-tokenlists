2e99f9ab2e71d4533e8ee16797e96f6f9a5e8a85
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-02-07 21:26:14 -0500

    test: suppress health check warning for too slow on schema fuzzing

--- tests/test_schema_fuzzing.py
+++ tests/test_schema_fuzzing.py
@@ -1,6 +1,6 @@
 import requests
 
-from hypothesis import given
+from hypothesis import given, settings, HealthCheck
 from hypothesis_jsonschema import from_schema
 
 from tokenlists import TokenList
@@ -9,5 +9,6 @@ TOKENLISTS_SCHEMA = "https://uniswap.org/tokenlist.schema.json"
 
 
 @given(token_list=from_schema(requests.get(TOKENLISTS_SCHEMA).json()))
+@settings(suppress_health_check=(HealthCheck.too_slow,))
 def test_schema(token_list):
     assert TokenList.from_dict(token_list).to_dict() == token_list
