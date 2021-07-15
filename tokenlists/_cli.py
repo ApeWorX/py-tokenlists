dc35dd77deaeb5b43a8888ba2f4cb096a5971851
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-07-15 15:52:23 -0400

    refactor: create manager object instead of working with globals

--- tokenlists/_cli.py
+++ tokenlists/_cli.py
@@ -1,7 +1,17 @@
 import click
 import yaml
 
-import tokenlists
+from .manager import TokenListManager
+from .typing import TokenSymbol
+
+
+class TokenlistChoice(click.Choice):
+    def __init__(self, case_sensitive=True):
+        self.case_sensitive = case_sensitive
+
+    @property
+    def choices(self):
+        return list(TokenListManager().available_tokenlists())
 
 
 @click.group()
@@ -11,38 +21,57 @@ def cli():
     """
 
 
-@cli.command(short_help="Display the names and versions of all installed tokenlists")
-def show_lists():
-    click.echo("Installed Token Lists:")
-    for token_list in map(tokenlists.get_token_list, tokenlists.available_token_lists()):
-        click.echo(f"- {token_list.name} (v{token_list.version})")
+@cli.command(name="list", short_help="Display the names and versions of all installed tokenlists")
+def _list():
+    manager = TokenListManager()
+
+    available_tokenlists = manager.available_tokenlists()
+    if available_tokenlists:
+        click.echo("Installed Token Lists:")
+        for tokenlist in map(manager.get_tokenlist, available_tokenlists):
+            click.echo(f"- {tokenlist.name} (v{tokenlist.version})")
+
+    else:
+        click.echo("WARNING: No tokenlists exist!")
 
 
-@cli.command(short_help="Display the info for a particular token")
-@click.argument("symbol", type=tokenlists.typing.TokenSymbol)
-@click.option(
-    "--token-list",
-    type=click.Choice(tokenlists.available_token_lists()),
-    default=tokenlists.default_token_list(),
-)
-def token_info(symbol, token_list):
-    token_info = tokenlists.get_token_info(symbol, token_list)
-    click.echo(yaml.dump(token_info.to_dict()))
-
-
-@cli.command(short_help="Install a new token list")
+@cli.command(short_help="Install a new tokenlist")
 @click.argument("uri")
 def install(uri):
-    tokenlists.install_token_list(uri)
+    manager = TokenListManager()
+
+    manager.install_tokenlist(uri)
 
 
-@cli.command(short_help="Remove an existing token list")
-@click.argument("name", type=click.Choice(tokenlists.available_token_lists()))
+@cli.command(short_help="Remove an existing tokenlist")
+@click.argument("name", type=TokenlistChoice())
 def remove(name):
-    tokenlists.uninstall_token_list(name)
+    manager = TokenListManager()
+
+    manager.remove_tokenlist(name)
 
 
 @cli.command(short_help="Set the default tokenlist")
-@click.argument("name", type=click.Choice(tokenlists.available_token_lists()))
+@click.argument("name", type=TokenlistChoice())
 def set_default(name):
-    tokenlists.set_default_token_list(name)
+    manager = TokenListManager()
+
+    manager.set_default_tokenlist(name)
+
+
+@cli.command(short_help="Display the info for a particular token")
+@click.argument("symbol", type=TokenSymbol)
+@click.option("--tokenlist-name", type=TokenlistChoice(), default=None)
+@click.option("--case-insensitive", default=False, is_flag=True)
+@click.option("--chain-id", default=1, type=int)
+def token_info(symbol, tokenlist_name, chain_id, case_insensitive):
+    manager = TokenListManager()
+
+    if not tokenlist_name:
+        if not manager.available_tokenlists():
+            raise click.ClickException("No tokenlists available!")
+
+        tokenlist_name = manager.default_tokenlist
+
+    token_info = manager.get_token_info(symbol, tokenlist_name, chain_id, case_insensitive)
+    click.echo(yaml.dump(token_info.to_dict()))
