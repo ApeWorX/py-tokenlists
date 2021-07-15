782303232ed921ebde3db8b19d949f564b7cbca1
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-07-15 18:58:01 -0400

    chore: upgrade to support Python 3.6+

--- setup.py
+++ setup.py
@@ -4,41 +4,50 @@
 from setuptools import find_packages, setup  # type: ignore
 
 extras_require = {
-    "test": [
-        "pytest==5.4.1",
-        "pytest-xdist",
-        "pytest-cov",
-        "PyGithub>=1.54,<2.0",
-        "hypothesis<6.0",
-        "hypothesis-jsonschema==0.19.0",
+    "test": [  # `test` GitHub Action jobs uses this
+        "pytest>=6.0,<7.0",  # Core testing package
+        "pytest-xdist",  # multi-process runner
+        "pytest-cov",  # Coverage analyzer plugin
+        "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
+        "PyGithub>=1.54,<2.0",  # Necessary to pull official schema from github
+        "hypothesis-jsonschema==0.19.0",  # Fuzzes based on a json schema
     ],
     "lint": [
-        "black==20.8b1",
-        "flake8==3.8.4",
-        "isort>=5.7.0,<6",
-        "mypy==0.790",
+        "black>=20.8b1,<21.0",  # auto-formatter and linter
+        "mypy>=0.800,<1.0",  # Static type analyzer
+        "flake8>=3.8.3,<4.0",  # Style linter
+        "isort>=5.7.0,<6.0",  # Import sorting linter
     ],
-    "doc": ["Sphinx>=3.4.3,<4", "sphinx_rtd_theme>=0.5.1"],
-    "release": [
-        "setuptools",
-        "setuptools-scm",
-        "wheel",
-        "twine",
+    "doc": [
+        "Sphinx>=3.4.3,<4",  # Documentation generator
+        "sphinx_rtd_theme>=0.1.9,<1",  # Readthedocs.org theme
+        "towncrier>=19.2.0, <20",  # Generate release notes
     ],
-    "dev": ["pytest-watch", "IPython", "ipdb"],
+    "release": [  # `release` GitHub Action job uses this
+        "setuptools",  # Installation tool
+        "setuptools-scm",  # Installation tool
+        "wheel",  # Packaging tool
+        "twine",  # Package upload tool
+    ],
+    "dev": [
+        "commitizen",  # Manage commits and publishing releases
+        "pre-commit",  # Ensure that linters are run prior to commiting
+        "pytest-watch",  # `ptw` test watcher/runner
+        "IPython",  # Console for interacting
+        "ipdb",  # Debugger (Must use `export PYTHONBREAKPOINT=ipdb.set_trace`)
+    ],
 }
 
 extras_require["dev"] = (
-    extras_require["dev"]
-    + extras_require["test"]
+    extras_require["test"]
     + extras_require["lint"]
-    + extras_require["release"]
     + extras_require["doc"]
+    + extras_require["release"]
+    + extras_require["dev"]
 )
 
-with open("README.md", "r") as fp:
-    long_description = fp.read()
-
+with open("./README.md") as readme:
+    long_description = readme.read()
 
 setup(
     name="tokenlists",
@@ -50,8 +59,10 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ApeWorX/py-tokenlists",
-    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    python_requires=">=3.6, <4",
     install_requires=[
+        "importlib-metadata ; python_version<'3.8'",
         "click>=8.0.0",
         "dataclassy>=0.10.3,<1.0",
         "pyyaml>=5.4.1,<6",
@@ -59,16 +70,23 @@ setup(
     ],
     entry_points={"console_scripts": ["tokenlists=tokenlists._cli:cli"]},
     extras_require=extras_require,
+    py_modules=["tokenlists"],
+    license="Apache-2.0",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"ape_tokens": ["py.typed"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
     ],
-    python_requires=">=3.7,<4",
 )
