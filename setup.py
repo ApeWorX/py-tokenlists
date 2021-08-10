4f791b179d88597019eace66cb61dde853118796
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-08-10 15:32:19 -0400

    chore: update linters and mypy

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ extras_require = {
         "hypothesis-jsonschema==0.19.0",  # Fuzzes based on a json schema
     ],
     "lint": [
-        "black>=20.8b1,<21.0",  # auto-formatter and linter
-        "mypy>=0.800,<1.0",  # Static type analyzer
-        "flake8>=3.8.3,<4.0",  # Style linter
-        "isort>=5.7.0,<6.0",  # Import sorting linter
+        "black>=21.7b0,<22.0",  # auto-formatter and linter
+        "mypy>=0.910,<1.0",  # Static type analyzer
+        "flake8>=3.9.2,<4.0",  # Style linter
+        "isort>=5.9.3,<6.0",  # Import sorting linter
     ],
     "doc": [
         "Sphinx>=3.4.3,<4",  # Documentation generator
