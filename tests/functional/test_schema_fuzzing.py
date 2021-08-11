429cdac8cb81f64657b916e0965c91b913ac57d2
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-08-10 20:58:51 -0400

    test: clean up iso timestamps so tests work

--- tests/functional/test_schema_fuzzing.py
+++ tests/functional/test_schema_fuzzing.py
@@ -9,12 +9,22 @@ from tokenlists import TokenList
 TOKENLISTS_SCHEMA = "https://uniswap.org/tokenlist.schema.json"
 
 
+def clean_iso_timestamps(tl: dict) -> dict:
+    """
+    Timestamps can be in any format, and our processing handles it okay
+    However, for testing purposes, we want the output format to line up,
+    and unfortunately there is some ambiguity in ISO timestamp formats.
+    """
+    tl["timestamp"] = tl["timestamp"].replace("Z", "+00:00")
+    return tl
+
+
 @pytest.mark.fuzzing
 @given(token_list=from_schema(requests.get(TOKENLISTS_SCHEMA).json()))
 @settings(suppress_health_check=(HealthCheck.too_slow,))
 def test_schema(token_list):
     try:
-        assert TokenList.parse_obj(token_list).dict() == token_list
+        assert TokenList.parse_obj(token_list).dict() == clean_iso_timestamps(token_list)
 
     except (ValidationError, ValueError):
         pass  # Expect these kinds of errors
